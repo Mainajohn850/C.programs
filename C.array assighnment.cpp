cf60ac71d3fa63949d09719516c7ecb0a7a99a7f
Mainajohn850 <kiunejohn98@gmail.com>
2024-11-15 10:38:34 +0300

    Add files via upload

---
+++ C.array assighnment.cpp
@@ -0,0 +1,26 @@
+//scores
+#include <stdio.h>
+
+int main(){
+	int j,k;
+	int scores[4][2]=
+	
+	{
+		
+	{65,92},
+	{84,72},
+	{35,70},
+	{59,67}
+	};
+	
+	for( j = 0;j < 4; j++){
+	for(k=0;k<2;k++){
+	
+	printf("scores [%d][%d]=%d\n",j,k,scores[j][k]);
+	}
+	}
+	return 0;
+		
+	
+	
+}
